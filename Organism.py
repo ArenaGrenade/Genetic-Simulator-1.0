4030eff413322c5911f305bf6e965e3523e3f3c1
ArenaGrenade <rohan.asokan@students.iiit.ac.in>
2021-12-14 08:17:12 +0400

    Added Organism Added Simulation Updated Render

--- Organism.py
+++ Organism.py
@@ -1,33 +1,87 @@
 import numpy as np
 
-#### Neural Network Inputs
-# Vision (1x5)
-# PheromoneGradA - Water
-# PheromoneGradB - Tree
-# PheromoneGradC - Berries
-# PheromoneA - Water
-# PheromoneB - Tree
-# PheromoneC - Berries
-# Age
-# Random
-# Oscillator - Daily
-# Last Movement - X, Y
-# Map Edge Distance - X, Y
-# Nearest Border Distance
-# World Location - X, Y
-####
-#### Neural Network Outputs
-# Set Oscillator Period
-# Emit Pheromone
-# Move - X, Y
-# Move Random
-# Use Mouth
-# Use Razor
+body_colors = {
+    0: [0, 0, 0, 0],
+    1: [0, 0, 0, 255],
+    2: [255, 165, 0, 255],
+    3: [221, 160, 221, 255]
+}
+
+class Neuron(object):
+    def __init__(self, enabled=False, output=0, name=None):
+        self.enabled = enabled
+        self.output = output
+        self.name = name
+        
+class Connection(object):
+    def __init__(self, sink, src, sink_id, src_id, weight=0):
+        self.sink = sink
+        self.sink_id = sink_id
+        self.source = src
+        self.src_id = src_id
+        self.weight = weight
+
+# Body Cells - None, Mover, Mouth, Wedge
 class Organism(object):
-    def __init__(self):
-        self.body = np.array((11, 11))
-        self.network = np.array(())
+    def __init__(self, spawn_loc, day_night_steps, rng):
+        self.rng = rng
+        self.body = np.zeros((11, 11), dtype=np.uint8)
+        self.body[5, 5] = 1
         
         self.age = 0
         self.food = 100
         self.water = 100
+        self.wood = 0
+        
+        self.oscillator_period = day_night_steps
+        self.oscillator_state = 0
+        
+        self.location = spawn_loc
+        self.last_movement = None
+        
+        self.input_names = ["I_Age", "I_Food", "I_Water", "I_Wood", "I_Oscillator", "I_WorldX", "I_WorldY", "I_LastX", "I_Last Y", "I_Rand", "I_BorderX", "I_BorderY", "I_BorderMin", "I_PhGradWX", "I_PhGradWY", "I_PhW", "I_PhGradTX", "I_PhGradTY", "I_PhT", "I_PhGradBX", "I_PhGradBY", "I_PhB", "I_IsDay", "I_IsSummer", "I_TimeStep", "I_DayNum"]
+        self.outputs_names = ["O_OscSet", "O_EmitPh", "O_MoveX", "O_MoveY", "O_MoveRand",  "O_Mouth", "O_Razor"]
+        self.neurons = [Neuron(enabled=True, name=name) for name in self.outputs_names]
+        self.connections = []
+    
+    def render_body(self):
+        rendered_org = np.zeros((*self.body.shape, 4), np.uint8)
+        
+        for col_idx, color in body_colors.items():
+            rendered_org[self.body == col_idx] = color
+        
+        return rendered_org
+
+    def forward(self, max_loc, waterph, treeph, berryph, is_day, is_summer, time_step, day_num):
+        # Handle Oscillator input
+        oscillator_input = 0
+        if self.oscillator_state >= self.oscillator_period:
+            oscillator_input = 1
+            self.oscillator_state = 0
+        else: self.oscillator_state += 1
+        
+        # Construct the input matrix
+        border_dist = [min(x, max_loc - x) for x in self.location]
+        inputs = np.array([self.age, self.food, self.water, self.wood, oscillator_input, *self.location, *self.last_movement, self.rng.random(), *border_dist, min(border_dist), *waterph, *treeph, *berryph, is_day, is_summer, time_step, day_num])
+        input_maximas = np.array([16 * 96, 1000, 1000, 300, self.oscillator_period, max_loc, max_loc, 1, 1, 1, max_loc, max_loc, max_loc, 10, 10, 10, 10, 10, 10, 10, 10, 10, 1, 1, 96, 32])
+        inputs_scaled = inputs / input_maximas
+        
+        # Initialize state variables
+        action_map = np.zeros(len(self.outputs_names))
+        neuron_acc = np.zeros(len(self.neurons))
+        outputs_computed = False
+        
+        # Perform feed forward
+        for conn in self.connections:
+            if conn.sink == "out" and not outputs_computed:
+                for neuron, acc in zip(self.neurons, neuron_acc):
+                    if neuron.enabled: neuron.output = np.tanh(acc)
+                outputs_computed = True
+            
+            if conn.source == "inp": input_val = inputs[conn.src_id]
+            else: input_val = self.neurons[conn.src_id].output
+            
+            if conn.sink == "out": action_map[conn.sink_id] += input_val * conn.weight
+            else: neuron_acc += input_val * conn.weight
+        
+        return action_map
--- Simulation.py
+++ Simulation.py
@@ -1,6 +1,7 @@
 import numpy as np
 from flask.json import JSONEncoder
 from sklearn.datasets import make_blobs
+from Organism import Organism
 
 map_colors = {
     0: [126, 200, 80, 255],
@@ -11,29 +12,49 @@ map_colors = {
 
 class Simulator(object):
     def __init__(self, gx, seed):
-        self.organisms = []
+        self.gx = gx
+        
         self.seed = seed
         self.rng = np.random.default_rng(seed)
         self.u, self.v = np.meshgrid(np.arange(gx), np.arange(gx))
         self.game_map = self.init_rand_map(gx)
         
+        self.organisms = []
+        self.init_organisms(gx)
+        
         self.time_step = 0
+        self.day_num = 0
         self.is_summer = True
         self.is_day = True
         
-    def make_clustered_spawns(self, num_cent, num, closeness, gx):
-        spawn_locs = np.stack([self.rng.integers(0, gx, size=num_cent), self.rng.integers(0, gx, size=num_cent)]).T
+        self.waterph_map = np.zeros((gx, gx), dtype=np.float32)
+        self.treeph_map = np.zeros((gx, gx), dtype=np.float32)
+        self.berryph_map = np.zeros((gx, gx), dtype=np.float32)
+    
+    def init_organisms(self):
+        for i in range(0, self.gx, 11):
+            for j in range(0, self.gx, 11):
+                if np.sum(self.game_map[i:i+11,j:j+11]) == 0:
+                    self.organisms.append(Organism(
+                        (i, j),
+                        96,
+                        self.rng
+                    ))
+        print(len(self.organisms))
+        
+    def make_clustered_spawns(self, num_cent, num, closeness):
+        spawn_locs = np.stack([self.rng.integers(0, self.gx, size=num_cent), self.rng.integers(0, self.gx, size=num_cent)]).T
         zone_indices = make_blobs(n_samples=num, centers=spawn_locs, cluster_std=closeness, random_state=self.seed)[0].astype(int)
-        zones = np.zeros((gx, gx), dtype=bool)
+        zones = np.zeros((self.gx, self.gx), dtype=bool)
         for x, y in zone_indices: 
             try: zones[x, y] = True
             except: continue
         return zones
 
-    def generate_water_blobs(self, gx):
+    def generate_water_blobs(self):
         radius1 = self.rng.integers(10, 25)
         radius2 = self.rng.integers(20, 40)
-        center1 = self.rng.integers(radius1, (gx - radius1, gx - radius1))
+        center1 = self.rng.integers(radius1, (self.gx - radius1, self.gx - radius1))
         c2theta, c2rad = -2 * np.pi * (self.rng.random() * 360), (radius1 + 10)
         center2 = [center1[0] + c2rad * np.cos(c2theta), center1[1] + c2rad * np.sin(c2theta)]
         
@@ -44,15 +65,15 @@ class Simulator(object):
         dist_scene = dist_circ1 * (1 - h) + dist_circ2 * h - k * h * (1 - h)
         return dist_scene <= 0
         
-    def init_rand_map(self, gx):
+    def init_rand_map(self):
         # 0 is land
         # 1 is water
         # 2 is food
         # 3 is trees
-        map = np.zeros((gx, gx), dtype=np.uint8)
-        map[self.make_clustered_spawns(20, 150, 4, gx)] = 3 # Spawn tree pods
-        map[self.make_clustered_spawns(30, 150, 25, gx)] = 2 # Spawn food pods
-        map[self.generate_water_blobs(gx)] = 1 # Create the water blob
+        map = np.zeros((self.gx, self.gx), dtype=np.uint8)
+        map[self.make_clustered_spawns(20, 150, 4)] = 3 # Spawn tree pods
+        map[self.make_clustered_spawns(30, 150, 25)] = 2 # Spawn food pods
+        map[self.generate_water_blobs()] = 1 # Create the water blob
         return map
     
     def render_map(self):
@@ -62,14 +83,70 @@ class Simulator(object):
         for col_idx, color in map_colors.items():
             map_render[self.game_map == col_idx] = color
         
+        # Render the organisms
+        for organism in self.organisms:
+            x, y = organism.location
+            body_render = organism.render_body()
+            map_render[x:x+11, y:y+11][organism.body != 0] = body_render[organism.body != 0]
+        
         return map_render.ravel().tolist()
+    
+    def simulate_step(self):
+        # Time related Variables Update
+        if self.time_step >= 96:
+            self.time_step = 0
+            self.day_num += 1
+        elif 0 <= self.time_step <= 48: 
+            self.is_day = True
+            self.time_step += 1
+        else:
+            self.is_day = False
+            self.time_step += 1
+        
+        if self.day_num >= 32:
+            self.day_num = 0
+        elif (self.day_num % 8) < 4: self.is_summer = True
+        else: self.is_summer = False
+        
+        # Simulate organisms for 1 time step
+        for organism in self.organisms:
+            action_map = organism.forward(self.gx, (0, 0, 0), (0, 0, 0), (0, 0, 0), self.is_day, self.is_summer, self.time_step, self.day_num)
+            for action, value in zip(organism.output_names, action_map):
+                if action == "O_EmitPh":
+                    continue
+                elif action == "O_MoveX" and np.abs(value) >= 0.5:
+                    organism.location[0] += np.sign(value)
+                elif action == "O_MoveY" and np.abs(value) >= 0.5:
+                    organism.location[1] += np.sign(value)
+                elif action == "O_MoveRand" and (1 / (1 + np.exp(-value))) >= 0.5:
+                    organism.location[0] += self.rng.integers(-1, 2)
+                    organism.location[1] += self.rng.integers(-1, 2)
+                elif action == "O_Mouth":
+                    continue
+                elif action == "O_Razor":
+                    continue
+        
+        # Update organism collision avoidance
+        for _ in range(3):
+            for organismA in self.organisms:
+                for organismB in self.organisms:
+                    dist = [a - b for a, b in zip(organismA.location, organismB.location)]
+                    if all(x <= 11 for x in dist):
+                        orgAmap = organismA.body[:-dist[0], :-dist[1]]
+                        orgBmap = organismB.body[:dist[0], :dist[1]]
+                        print(orgAmap.shape, orgBmap.shape)
+                        print(orgAmap + orgBmap)
+                        
+        # Update organism reproduction
+        
+        # Update organism death
 
 class CustomJSONEncoder(JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Simulator):
             return {
                 'gameMap': obj.game_map.tolist(),
-                "organisms": obj.organisms,
+                # "organisms": obj.organisms,
                 'timeStep': obj.time_step,
                 'isSummer': obj.is_summer,
                 "isDay": obj.is_day,
